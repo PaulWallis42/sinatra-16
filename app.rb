4e12be180285a1d72dad7318a85be454c04ffd1c
paulwallis42 <paulwallis42@gmail.com>
2016-04-26 13:03:53 +0100

    params

--- app.rb
+++ app.rb
@@ -10,7 +10,12 @@ get '/secret' do
   'This is a secret page'
 end
 
-get '/cat' do
+get '/random-cat' do
   @cat_name = ['Amigo', 'Oscar', 'Viking'].sample
+  erb :index
+end
+
+get '/named-cat' do
+  @cat_name = params[:name]
   erb :index
 end
